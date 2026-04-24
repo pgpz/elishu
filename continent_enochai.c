7711f6249e6708bcc16b154cb3532b2be2105701
pgp <admin@pgpkey.net>
2026-04-24 16:26:58 -0700

    enochai ideas

---
+++ continent_enochai.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enochai - STARTING PLACE; ANCIENT CONTINENT magical fantasy-like continent on the planet Elishu in the Andromeda Galaxy;\n");
+    printf("consists of palm trees, hot springs, stone path walkways, italian-styled housing, japanese-styled housing, magical waterfalls BY shrines that can be used to transport to Terra(Earth), always twilight because it is a tidally locked planet not facing directly to the home-star;\n");
+    printf("due to the massive midnight-coloured clouds and gasses, this planet gradually changes from {Nautical Twilight(mid-length) -> Astronomical Twilight(longest-length) -> Civil Twilight(shortest-length)}, tribal food consists of magical fruits that promote healthy and lengthy life-spans, the foods that are grown here can only grow in Enochai\n");
+    printf("\n");
+    printf("Nautical Twilight - Suns center is 6 degrees - 12 degrees below horizon\n");
+    printf("Astronomical Twilight - Suns center is 12 degrees - 18 degrees below horizon\n");
+    printf("Civil Twilight - Suns center 0 degrees - 6 degrees below horizon\n");
+
+    return 0;
+}
